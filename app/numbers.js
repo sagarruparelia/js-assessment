6adf53ee097ad75f37717ecd93af6673441823ea
sagar <ruparelia.sagar@gmail.com>
2016-08-09 15:33:54 -0400

    numbers done

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var result = (num >> (bit-1)).toString(2);
+    return parseInt(result.charAt(result.length -1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    var pad = "00000000",
+        ans = num.toString(2);
+    return pad.substring(0, pad.length - ans.length) + ans;
   },
 
   multiply: function(a, b) {
 
+    return parseFloat((a*b).toFixed((b + "").split(".")[1].length));
   }
 };
